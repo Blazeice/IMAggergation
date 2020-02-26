6c27b28e8242ef1fd1269a1ebf59a2fb5c8e48ae
blazeice <blazeiceb@gmail.com>
2020-02-26 17:02:21 +0800

    first commit

---
+++ IMAggergation/Classes/IMAggergationManager.h
@@ -0,0 +1,17 @@
+//
+//  IMAggergationManager.h
+//  
+//
+//  Created by bingbai on 2020/2/26.
+//  Copyright © 2020 bingbai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IMAggergationManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IMAggergation/Classes/IMAggergationManager.m
@@ -0,0 +1,19 @@
+//
+//  IMAggergationManager.m
+//
+//
+//  Created by bingbai on 2020/2/26.
+//  Copyright © 2020 bingbai. All rights reserved.
+//
+
+#import "IMAggergationManager.h"
+
+@implementation IMAggergationManager
+
+- (void)testPrint:(NSString *)text {
+    
+    NSLog(@"IMAggergationManager print == %@",text);
+    
+}
+
+@end
